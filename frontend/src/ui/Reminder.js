a6911c6f17c4a9be9ce258ba6d53e9c9c2f1fb55
Samuel Martinez <semartinez147@gmail.com>
2021-05-14 11:57:16 -0600

    finished static Reminder page

--- frontend/src/ui/Reminder.js
+++ frontend/src/ui/Reminder.js
@@ -1,7 +1,5 @@
 import React from "react"
-import {Col, Container, Form, Image, Navbar, Row, Spinner} from "react-bootstrap";
-import DayPickerInput from "react-day-picker/DayPickerInput";
-import 'react-day-picker/lib/style.css';
+import {Button, Col, Container, Form, Image, Navbar, Row} from "react-bootstrap";
 import logo from "../assets/logo-filler.png"
 
 export const Reminder = () => {
@@ -24,25 +22,31 @@ export const Reminder = () => {
                     <Col>
                         <h1 className="my-3">Schedule A Reminder</h1>
                         <Image src={logo} className="my-4 p-4 border border-dark"/>
+                        <h2>(Plant Name)</h2>
                     </Col>
                 </Row>
             </Container>
             <Container className="text-center">
                 <Form>
-                    <Row>
-                        <Col md={6}>
+                    <Row className="justify-content-center">
+                        <Col md={3} className="my-2">
                             <Form.Group controlId="datePicker">
                                 <Form.Label>When to start reminders:</Form.Label>
-                                {/*DayPicker docs at http://react-day-picker.js.org/examples/input*/}
-                                {/*<DayPickerInput as={Form.Control} type="date"  onDayChange={day => console.log(day)}/>*/}
-                                <Form.Control as={DayPickerInput} type="date" onDayChange={day => console.log(day)}/>
+                                <Form.Control type="date" placeholder="Number of days"/>
                             </Form.Group>
                         </Col>
-                        <Col md={6}>
+                        <Col md={3} className="my-2">
                             <Form.Group controlId="intervalPicker">
-                                <Form.Label>How often to be reminded:</Form.Label>
-                                <Form.Control type="number" placeholder="Number of days"/></Form.Group>
-                        </Col></Row>
+                                <Form.Label>Days between reminders:</Form.Label>
+                                <Form.Control type="number" htmlSize={4} min="1" max="365" placeholder="Number of days"/></Form.Group>
+                        </Col>
+                    </Row>
+                    <Row  className="justify-content-center">
+                        <Col md={6}>
+                            <Button variant="primary" size="lg" block>
+                                Save reminder </Button>
+                        </Col>
+                    </Row>
                 </Form>
             </Container>
         </>
