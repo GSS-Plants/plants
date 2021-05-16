513f5f79f91ac114ce814c68a84e286072a8b7ae
Samuel Martinez <semartinez147@gmail.com>
2021-05-16 10:20:40 -0600

    Navbar finished

--- frontend/src/ui/PlantrNav.js
+++ frontend/src/ui/PlantrNav.js
@@ -18,30 +18,21 @@ export const PlantrNav = () => {
                     plant.r
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbar-links"/>
-                <Navbar.Collapse id="navbar-links">
-                    <Container fluid className="px-0">
-                        <Row className="w-100">
-                            <Col>
-                                <Nav className="ms-auto">
-                                    <Nav.Link href="/reminder">Reminder</Nav.Link>
-                                    <Nav.Link href="#search">Search</Nav.Link>
-                                    <Nav.Link href="#plant">Plant</Nav.Link>
-                                    <Nav.Link href="#greenhouse">Greenhouse</Nav.Link>
-                                    <Nav.Link href="#contact">Contact Team Plants</Nav.Link>
-                                </Nav>
-                            </Col>
-                            <Col xl={5} className="justify-content-between">
-                                <Form inline as={InputGroup}>
-                                <Form.Control type="text" placeholder="Search for plants"/>
-                                <InputGroup.Append><Button variant="outline-success">Go</Button></InputGroup.Append>
-                            </Form>
-                            </Col>
-                            <Col xs={{order: "first"}} xl={{span: 2, order: "last"}} className="px-xl-0">
-                                <Button variant="success">Sign in/Sign up</Button>
-                            </Col>
-                        </Row>
-                    </Container>
+                <Navbar.Collapse id="navbar-links" className="justify-content-between">
+                    <Nav className="justify-content-between">
+                        <Nav.Link href="/reminder">Reminder</Nav.Link>
+                        <Nav.Link href="#search">Search</Nav.Link>
+                        <Nav.Link href="#plant">Plant</Nav.Link>
+                        <Nav.Link href="#greenhouse">Greenhouse</Nav.Link>
+                        <Nav.Link href="#contact">Contact Team Plants</Nav.Link>
+                        <Nav.Link href="#loginModal" className="text-success">Sign up / Sign in</Nav.Link>
+                    </Nav>
+                    <Form inline as={InputGroup} className="w-50">
+                        <Form.Control type="text" placeholder="Search for plants"/>
+                        <InputGroup.Append><Button variant="outline-success">Go</Button></InputGroup.Append>
+                    </Form>
                 </Navbar.Collapse>
+
             </Navbar>
         </>
     )
