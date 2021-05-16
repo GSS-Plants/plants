9ca6df071241aac1cca090c641d5d2f61d980453
Massimo Bouchentouf <mbouchentouf@cnm.edu>
2021-05-16 17:49:40 -0600

    added card and search image

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Route, Switch } from 'react-router'
 import { Home } from './Home'
+import { Search } from './Search'
 import { Reminder } from './Reminder';
 import { FourOhFour } from './pagesFourOhFour'
 import React from 'react'
@@ -10,7 +11,7 @@ export const App = () => (
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/reminder' component={Reminder} />
-                {/*<Route exact path='/search' component={Search} />*/}
+                <Route exact path='/search' component={Search} />
                 {/*<Route exact path='/plant' component={Plant} />*/}
                 {/*<Route exact path='/greenhouse' component={Greenhouse} />*/}
                 {/*<Route exact path='/contact' component={Contact} />*/}
---
+++ frontend/src/ui/Search.js
@@ -0,0 +1,34 @@
+import React from "react"
+import {Col, Container, Row, Card, ListGroup, ListGroupItem} from "react-bootstrap";
+
+export const Search = () => {
+    return (
+        <>
+            <Container>
+
+
+            <h1><b>Search</b></h1>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="https://www.calloways.com/wp-content/uploads/grafted-cactus.jpg" />
+                <Card.Body>
+                    <img src="https://miro.medium.com/max/1106/1*tAZ0DsBYgXTsn2BBLxlIIg.png" className={'img-fluid'} />
+                    <Card.Title>Search</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem>Cras justo odio</ListGroupItem>
+                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+            </Container>
+        </>
+    )
+}
