2741b99490095b0c537b359dc4e36c17147e750e
Gabriella Hawkins <ghawkins3@cnm.edu>
2021-05-18 21:44:56 -0600

    finished static home

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -1,13 +1,60 @@
-import React from "react"
-import {Col, Container, Row} from "react-bootstrap";
-export const Home = () => {
+import React from 'react'
+import {Row, Col, Jumbotron, Button, Form, Card, ListGroup, ListGroupItem, Navbar} from 'react-bootstrap'
+import "./Styles.css"
+import logo from "../assets/logo-filler.png";
 
+export const Home = () => {
     return (
-        <Container fluid id="TheCont">
+        <div id="container">
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src={logo}
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    Replace me with the real Navbar
+                </Navbar.Brand>
+            </Navbar>
             <Row>
-                <Col  lg={{span:8, offset:2}}>
-                    <img src="https://images.unsplash.com/photo-1485841890310-6a055c88698a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="plantphoto" id="photoplants"/>
+                <Col>
+                    <Jumbotron className="jumbotron jumbotron-fluid">
+                    </Jumbotron>
                 </Col>
             </Row>
-            
-        </Container>)}
+            <Row>
+                <Col id="cardA">
+                    <Card style={{width: '18rem'}} id="cardd">
+                        <Card.Img variant="top" src="https://images.unsplash.com/photo-1509937528035-ad76254b0356?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=924&q=80"/>
+                        <Card.Body>
+                            <Card.Title>About Us!</Card.Title>
+                            <Card.Text>
+                              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam facere sunt veritatis.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                            <ListGroupItem>Samuel Martinez</ListGroupItem>
+                            <ListGroupItem>Kirsten Crocker</ListGroupItem>
+                            <ListGroupItem>Massimo Bouchentouf</ListGroupItem>
+                            <ListGroupItem>Gabriella Hawkins</ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+            <Row>
+                <Col lg={{span:10,offset:1}}>
+                    <Form.Group>
+                        <Form.Control size="lg" type="text" placeholder="What kind of plant do you have?"/>
+                    </Form.Group>
+                </Col>
+            </Row>
+            <Row>
+                <Col id="buttcall">
+                    <Button id="Butt">Learn More</Button>
+                </Col>
+            </Row>
+        </div>
+    )
+}
