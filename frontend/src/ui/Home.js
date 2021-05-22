3ba785b270885c749e721642153b1df80e116516
Gabriella Hawkins <ghawkins3@cnm.edu>
2021-05-21 21:51:43 -0600

    almost done

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -37,6 +37,8 @@ export const Home = () => {
                     </Jumbotron>
                 </Col>
             </Row>
+            <h2>This is how you use the site</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A autem, consequatur cumque debitis dignissimos dolore dolorum eius ex labore modi nihil numquam omnis quaerat qui quisquam ratione repellat, sunt unde! Animi culpa magni molestias tenetur! Dolorum neque officiis perspiciatis rem voluptate. Dolorem fuga minima quasi, recusandae repellendus rerum tenetur ullam. Deleniti ratione tempore unde voluptates. Adipisci asperiores autem eos ipsum minus nesciunt nulla quis veniam. Accusamus eius excepturi expedita illo maiores non nulla officiis porro possimus voluptas! Ab inventore, totam!</p>
             <Row>
                 <Col id="cardA">
                     <Card style={{width: '18rem'}} id="cardd">
