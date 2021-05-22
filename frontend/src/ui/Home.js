c56c78229a97648dd5a3f1bfed9ca2cfc3f47bbe
Gabriella Hawkins <ghawkins3@cnm.edu>
2021-05-21 21:29:20 -0600

    working on home page

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -5,7 +5,8 @@ import logo from "../assets/logo-filler.png";
 
 export const Home = () => {
     return (
-        <div id="container">
+        <div  id="container">
+
             <Navbar bg="dark" variant="dark">
                 <Navbar.Brand href="#home">
                     <img
@@ -18,43 +19,94 @@ export const Home = () => {
                     Replace me with the real Navbar
                 </Navbar.Brand>
             </Navbar>
+
             <Row>
-                <Col>
-                    <Jumbotron className="jumbotron jumbotron-fluid">
+                <Col >
+                    <Jumbotron id="jumone"   className="jumbotron jumbotron-fluid">
+                        <Jumbotron id="thejum">
+                        <h1 id="logo"><b>I WILL BE YOUR LOGO!!</b></h1>
+                        </Jumbotron>
+                        <Form.Group id="search">
+                            <Form.Control size="lg" type="text" placeholder="What kind of plant do you have?"/>
+                        </Form.Group>
+                        <Row>
+                            <Col id="buttcall">
+                                <Button id="Butt">Learn More</Button>
+                            </Col>
+                        </Row>
                     </Jumbotron>
                 </Col>
             </Row>
             <Row>
                 <Col id="cardA">
                     <Card style={{width: '18rem'}} id="cardd">
-                        <Card.Img variant="top" src="https://images.unsplash.com/photo-1509937528035-ad76254b0356?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=924&q=80"/>
+                        <Card.Img variant="top"
+                                  src="https://images.unsplash.com/photo-1509937528035-ad76254b0356?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=924&q=80"/>
                         <Card.Body>
+                            <ListGroupItem>Kirsten Crocker</ListGroupItem>
                             <Card.Title>About Us!</Card.Title>
                             <Card.Text>
-                              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam facere sunt veritatis.
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam facere sunt
+                                veritatis.
                             </Card.Text>
                         </Card.Body>
                         <ListGroup className="list-group-flush">
+                        </ListGroup>
+                    </Card>
+                </Col>
+
+                <Col id="cardA">
+                    <Card style={{width: '18rem'}} id="cardd">
+                        <Card.Img variant="top"
+                                  src="https://images.unsplash.com/photo-1509937528035-ad76254b0356?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=924&q=80"/>
+                        <Card.Body>
                             <ListGroupItem>Samuel Martinez</ListGroupItem>
-                            <ListGroupItem>Kirsten Crocker</ListGroupItem>
-                            <ListGroupItem>Massimo Bouchentouf</ListGroupItem>
+                            <Card.Title>About Us!</Card.Title>
+                            <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam facere sunt
+                                veritatis.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                        </ListGroup>
+                    </Card>
+                </Col>
+
+                <Col id="cardA">
+                    <Card style={{width: '18rem'}} id="cardd">
+                        <Card.Img variant="top"
+                                  src="https://images.unsplash.com/photo-1509937528035-ad76254b0356?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=924&q=80"/>
+                        <Card.Body>
+                            <ListGroupItem>Simo</ListGroupItem>
+                            <Card.Title>About Us!</Card.Title>
+                            <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam facere sunt
+                                veritatis.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
+                        </ListGroup>
+                    </Card>
+                </Col>
+
+                <Col id="cardA" class="col-md-3 mx-auto mb-5 text-center">
+                    <Card style={{width: '18rem'}} id="cardd">
+                        <Card.Img variant="top"
+                                  src="https://images.unsplash.com/photo-1509937528035-ad76254b0356?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=924&q=80"/>
+                        <Card.Body>
                             <ListGroupItem>Gabriella Hawkins</ListGroupItem>
+                            <Card.Title>About Us!</Card.Title>
+                            <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aperiam facere sunt
+                                veritatis.
+                            </Card.Text>
+                        </Card.Body>
+                        <ListGroup className="list-group-flush">
                         </ListGroup>
                     </Card>
                 </Col>
             </Row>
-            <Row>
-                <Col lg={{span:10,offset:1}}>
-                    <Form.Group>
-                        <Form.Control size="lg" type="text" placeholder="What kind of plant do you have?"/>
-                    </Form.Group>
-                </Col>
-            </Row>
-            <Row>
-                <Col id="buttcall">
-                    <Button id="Butt">Learn More</Button>
-                </Col>
-            </Row>
+
         </div>
     )
 }
