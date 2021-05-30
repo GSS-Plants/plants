434fbaf2a56d5d8efc128e2505b15ee391616d03
Gabriella Hawkins <ghawkins3@cnm.edu>
2021-05-30 17:42:23 -0600

    built signup validator

---
+++ backend/src/apis/sign-up/signup.validator.ts
@@ -0,0 +1,26 @@
+export const signupValidator = {
+    profileEmail: {
+        isEmail: {
+            errorMessage: 'Please provide a valid email'
+        },
+        // Uncomment the next line to sanitize email, but it removes +1 from testing email addresses.
+        // normalizeEmail: true,
+        trim: true
+    },
+    profilePassword: {
+        isLength: {
+            errorMessage: 'Password must be at least eight characters',
+            options: { min: 8 }
+        },
+        trim: true,
+        escape: true
+    },
+    profilePasswordConfirm: {
+        isLength: {
+            errorMessage: 'confirm password must be at least eight characters',
+            options: { min: 8 }
+        },
+        trim: true,
+        escape: true
+    },
+};
