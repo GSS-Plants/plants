9a71efce369cd020ee665e5f665f5abfda2043ec
Samuel Martinez <semartinez147@gmail.com>
2021-06-03 15:46:33 -0600

    working on data downloader

--- backend/src/utils/data-downloader/plantr-loader.ts
+++ backend/src/utils/data-downloader/plantr-loader.ts
@@ -1,4 +1,6 @@
 import axios from "axios";
+import {Profile} from "../interfaces/Profile";
+import uuid from "uuid";
 
 function plantrLoader() : Promise<any> {
     async function main() {
@@ -12,10 +14,15 @@ function plantrLoader() : Promise<any> {
     return main();
 
     async function downloadUsers() {
+        const results: any = []
         try {
             const userRequest = await axios.get("https://jsonplaceholder.typicode.com/users")
             for (const user of userRequest.data) {
-                console.log(user.userName)
+                const profile: Profile = {
+                    profileId: uuid(),
+                    profileEmail: user.email,
+                    profileLogin: user.username,
+                }
             }
         } catch (error) {
             throw error;
