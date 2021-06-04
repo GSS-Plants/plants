7e4f860131ec61d725b6bc24cf060272eca0d4ec
Massimo Bouchentouf <mbouchentouf@cnm.edu>
2021-06-04 11:33:06 -0600

    cleaned up commited files

--- frontend/src/ui/Search.js
+++ frontend/src/ui/Search.js
@@ -10,7 +10,7 @@ import {
     Nav,
     Form,
     InputGroup,
-    Button
+    Button, CardColumns, CardDeck
 } from "react-bootstrap";
 import logo from "../assets/logo-filler.png";
 
@@ -33,7 +33,7 @@ export const Search = () => {
                     <Navbar.Collapse id="navbar-links" className="justify-content-between">
                         <Nav className="justify-content-between">
                             <Nav.Link href="/reminder">Reminder</Nav.Link>
-                            <Nav.Link href="#search">Search</Nav.Link>
+                            <Nav.Link href="/search">Search</Nav.Link>
                             <Nav.Link href="#plant">Plant</Nav.Link>
                             <Nav.Link href="#greenhouse">Greenhouse</Nav.Link>
                             <Nav.Link href="#contact">Contact Team Plants</Nav.Link>
@@ -48,25 +48,72 @@ export const Search = () => {
                 </Navbar>
 
             <h1><b>Search</b></h1>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://www.calloways.com/wp-content/uploads/grafted-cactus.jpg" />
+            <CardDeck>
+                <Card style={{ width: '18rem' }}>
                 <Card.Body>
-                    <Card.Title>Search</Card.Title>
+                    <Card.Title>plantCommonName</Card.Title>
                     <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
+                        <b>plantScientificName</b>
                     </Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                    <ListGroupItem>Cras justo odio</ListGroupItem>
-                    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+                    <ListGroupItem>plantDuration</ListGroupItem>
+                    <ListGroupItem>plantGrowthHabit</ListGroupItem>
+                    <ListGroupItem>plantToxicity</ListGroupItem>
                 </ListGroup>
                 <Card.Body>
                     <Card.Link href="#">More Details</Card.Link>
-                    <Card.Link href="#">Another Search</Card.Link>
                 </Card.Body>
             </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>plantCommonName</Card.Title>
+                        <Card.Text>
+                            <b>plantScientificName</b>
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem>plantDuration</ListGroupItem>
+                        <ListGroupItem>plantGrowthHabit</ListGroupItem>
+                        <ListGroupItem>plantToxicity</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body>
+                        <Card.Link href="#">More Details</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>plantCommonName</Card.Title>
+                        <Card.Text>
+                            <b>plantScientificName</b>
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem>plantDuration</ListGroupItem>
+                        <ListGroupItem>plantGrowthHabit</ListGroupItem>
+                        <ListGroupItem>plantToxicity</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body>
+                        <Card.Link href="#">More Details</Card.Link>
+                    </Card.Body>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                        <Card.Title>plantCommonName</Card.Title>
+                        <Card.Text>
+                            <b>plantScientificName</b>
+                        </Card.Text>
+                    </Card.Body>
+                    <ListGroup className="list-group-flush">
+                        <ListGroupItem>plantDuration</ListGroupItem>
+                        <ListGroupItem>plantGrowthHabit</ListGroupItem>
+                        <ListGroupItem>plantToxicity</ListGroupItem>
+                    </ListGroup>
+                    <Card.Body>
+                        <Card.Link href="#">More Details</Card.Link>
+                    </Card.Body>
+                </Card>
+            </CardDeck>
             </Container>
         </>
     )
