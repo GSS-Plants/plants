1b62120fea992453ae2f616a3e1cf43147673b0f
Samuel Martinez <semartinez147@gmail.com>
2021-06-08 13:52:33 -0600

    get profilePlant by profilePlantId route works

--- backend/src/utils/profile-plant/selectProfilePlantByProfilePlantId.ts
+++ backend/src/utils/profile-plant/selectProfilePlantByProfilePlantId.ts
@@ -4,11 +4,9 @@ import {ProfilePlant} from "../interfaces/ProfilePlant";
 export async function selectProfilePlantByProfilePlantId(profilePlantId: string) {
     try {
         const mysqlConnection = await connect();
-        const query : string = 'SELECT profilePlantId, profilePlantPlantId, profilePlantProfileId, profilePlantNotes FROM profilePlant WHERE profilePlantId = UUID_TO_BIN(:profilePlantId);'
+        const query : string = "SELECT BIN_TO_UUID(profilePlantId) AS profilePlantId, BIN_TO_UUID(profilePlantPlantId) AS profilePlantPlantId, BIN_TO_UUID(profilePlantProfileId) AS profilePlantProfileId, profilePlantNotes FROM profilePlant WHERE profilePlantId = UUID_TO_BIN(:profilePlantId);"
 
-        const [rows] = await mysqlConnection.query(query, profilePlantId);
-        // @ts-ignore
-        console.log(rows[0])
+        const [rows] = await mysqlConnection.execute(query, {profilePlantId});
         // @ts-ignore is required so that rows can be interacted with like the array it is
         return rows[0]
     } catch (e) {
