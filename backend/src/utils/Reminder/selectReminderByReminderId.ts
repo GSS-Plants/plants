7f930439ee06a8d75171855388b4c4c316c7fb1f
Samuel Martinez <semartinez147@gmail.com>
2021-06-14 13:34:38 -0600

    added reminder generation to seed-data.ts, confirmed GET, PUT and POST routes working.

--- backend/src/utils/Reminder/selectReminderByReminderId.ts
+++ backend/src/utils/Reminder/selectReminderByReminderId.ts
@@ -3,11 +3,12 @@ import {Reminder} from "../interfaces/Reminder";
 
 export async function selectReminderByReminderId(reminderId: string) {
     try {
+        console.log('selectReminderByReminderId: ', reminderId)
         const mysqlConnection = await connect();
-        const query : string = 'SELECT UUID_TO_BIN(reminderId) AS reminderId, UUID_TO_BIN(reminderProfilePlantId) AS reminderProfilePlantId, reminderDescription, reminderRecurrence, reminderStartDate FROM reminder WHERE reminderId = :reminderId';
-
+        const query : string = "SELECT BIN_TO_UUID(reminderId) AS reminderId, BIN_TO_UUID(reminderProfilePlantId) AS reminderProfilePlantId, reminderDescription, reminderRecurrence, DATE_FORMAT(reminderStartDate, '%Y-%m-%e') AS reminderStartDate FROM reminder WHERE reminderId = UUID_TO_BIN(:reminderId)";
         const [rows] = await mysqlConnection.execute(query, {reminderId});
-        return rows
+        //@ts-ignore
+        return rows[0]
     } catch (e) {
         console.error(e)
         return null
--- backend/src/utils/interfaces/Reminder.ts
+++ backend/src/utils/interfaces/Reminder.ts
@@ -1,7 +1,14 @@
+export interface UpdateReminder {
+    reminderDescription: string|null,
+    reminderRecurrence: number|null,
+    reminderStartDate: string|null
+}
+
 export interface Reminder {
     reminderId: string,
     reminderProfilePlantId: string,
     reminderDescription: string|null,
     reminderRecurrence: number|null,
-    reminderStartDate: number,
+    reminderStartDate: string
 }
+
