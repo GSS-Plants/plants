b9d2d332b5970eb88c124b6e09a41c3d9d6aa237
Samuel Martinez <semartinez147@gmail.com>
2021-06-14 14:37:05 -0600

    Redux into Reminder.js is working.  Need to insert Formik and wire form to create a new reminder.

--- backend/src/utils/Reminder/selectReminderByProfileId.ts
+++ backend/src/utils/Reminder/selectReminderByProfileId.ts
@@ -4,9 +4,11 @@ import {connect} from "../database.utils";
 export async function selectReminderByProfileId(profileId: string) {
     try {
         const mysqlConnection = await connect();
-        const query : string = 'SELECT BIN_TO_UUID(reminderId) AS reminderId, BIN_TO_UUID(reminderProfilePlantId) AS reminderProfilePlantId, reminderDescription, reminderRecurrence, reminderStartDate FROM reminder INNER JOIN profilePlant ON reminder.reminderProfilePlantId = profilePlant.profilePlantId WHERE profilePlant.profilePlantProfileId = UUID_TO_BIN(:profileId)'
+        const query : string = "SELECT BIN_TO_UUID(reminderId) AS reminderId, BIN_TO_UUID(reminderProfilePlantId) AS reminderProfilePlantId, plant.plantCommonName,reminderDescription, reminderRecurrence, DATE_FORMAT(reminderStartDate, '%m/%d/%Y') AS reminderStartDate FROM reminder INNER JOIN profilePlant ON reminder.reminderProfilePlantId = profilePlant.profilePlantId INNER JOIN plant on  profilePlant.profilePlantPlantId = plant.plantId WHERE profilePlant.profilePlantProfileId = UUID_TO_BIN(:profileId)"
 
         const [rows] = await mysqlConnection.execute(query, {profileId});
+        // @ts-ignore is required so that rows can be interacted with like the array it is
+        console.log(rows[0])
         // @ts-ignore is required so that rows can be interacted with like the array it is
 
         return rows
