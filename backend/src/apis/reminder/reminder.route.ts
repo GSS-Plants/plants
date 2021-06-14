a6912f59d7415cff762f7f8842154d3c3d9a9391
Samuel Martinez <semartinez147@gmail.com>
2021-06-14 13:34:38 -0600

    added reminder generation to seed-data.ts, confirmed GET, PUT and POST routes working.

--- backend/src/apis/reminder/reminder.route.ts
+++ backend/src/apis/reminder/reminder.route.ts
@@ -1,12 +1,15 @@
 import {Router} from "express";
 import {getReminderController, deleteReminderController, postReminderController, putReminderController, getReminderByProfileId} from "./reminder.controller";
+import {asyncValidatorController} from "../../utils/controllers/asyncValidator.controller";
+import {checkSchema} from "express-validator";
+import {reminderValidator} from "./reminder.validator";
 
 
 export const ReminderRoute = Router();
 
 
 ReminderRoute.route('/')
-    .post(postReminderController)
+    .post(asyncValidatorController(checkSchema(reminderValidator)), postReminderController)
 
 ReminderRoute.route('/:reminderId')
     .put(putReminderController)
--- backend/src/utils/Reminder/updateReminder.ts
+++ backend/src/utils/Reminder/updateReminder.ts
@@ -1,12 +1,12 @@
 import {connect} from "../database.utils";
-import {Reminder} from "../interfaces/Reminder";
+import {Reminder, UpdateReminder} from "../interfaces/Reminder";
 
 export async function updateReminder(newReminder: Reminder) {
     try {
         const mysqlConnection = await connect();
         const query : string = 'UPDATE reminder SET reminderDescription = :reminderDescription, reminderRecurrence = :reminderRecurrence, reminderStartDate = :reminderStartDate WHERE reminderId = UUID_TO_BIN(:reminderId)';
 
-        const [rows] = await mysqlConnection.execute(query, {newReminder});
+        const [rows] = await mysqlConnection.execute(query, newReminder);
 
         // @ts-ignore is required so that rows can be interacted with like the array it is
         return rows[0]
