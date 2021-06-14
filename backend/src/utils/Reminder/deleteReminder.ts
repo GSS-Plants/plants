66fd4886649de734f96ac484c67fc7cb5ca388d2
Samuel Martinez <semartinez147@gmail.com>
2021-06-14 13:41:39 -0600

    deleteReminder works

--- backend/src/utils/Reminder/deleteReminder.ts
+++ backend/src/utils/Reminder/deleteReminder.ts
@@ -4,10 +4,10 @@ import {Reminder} from "../interfaces/Reminder";
 export async function deleteReminder(reminderId: string) {
     try {
         const mysqlConnection = await connect();
-        const query : string = 'DELETE FROM reminder WHERE reminderId = :reminderId';
+        const query : string = 'DELETE FROM reminder WHERE reminderId = UUID_TO_BIN(:reminderId)';
 
-        const [rows] = await mysqlConnection.execute(query, {reminderId});
-        return 'Reminder deleted'
+        const result = await mysqlConnection.execute(query, {reminderId});
+        return 'Deleted ' + result + 'entries'
     } catch (e) {
         console.error(e)
         return null
